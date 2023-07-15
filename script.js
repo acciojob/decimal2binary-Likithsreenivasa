a4306c9ec0e95285f74b0b47eb4a40f8620802e4
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-15 08:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function decimalToBinary(num) {
 	
   //Write you code here
 	let sum=""
-	for(let i=0;i<num;i++)
+	if(num==0)
+	{
+		return 0;
+	}
+	while(num>0)
 		{
-			sum=sum+(num%2);
+			sum=(num%2)+sum;
 			num=Math.floor(num/2);
 		}
 	return sum;
